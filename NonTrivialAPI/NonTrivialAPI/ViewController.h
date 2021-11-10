faceb4dd373dafb34651f63281599a6bee50a44c
aoifem00 <amcmanu4@binghamton.edu>
2021-11-10 10:09:08 -0500

    Began adding vvisuals

--- NonTrivialAPI/NonTrivialAPI/ViewController.h
+++ NonTrivialAPI/NonTrivialAPI/ViewController.h
@@ -4,10 +4,11 @@
 //
 //  Created by Aoife McManus on 10/18/21.
 //
-
+#import <CoreLocation/CoreLocation.h>
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property(nonatomic, strong) CLLocationManager* manager;
 
 
 @end
--- NonTrivialAPI/NonTrivialAPI/ViewController.m
+++ NonTrivialAPI/NonTrivialAPI/ViewController.m
@@ -12,16 +12,9 @@
 @end
 
 @implementation ViewController
-/*
-- (NSMutableDictionary*)getTimes:(NSMutableArray*)arr{
-    NSMutableDictionary *dict=[[NSMutableDictionary alloc]init];
-    for(int i=1; i<arr.count-1; i++){
-        [dict setValue:@"placeholder" forKey:arr[i][1]];
-    }
-    return dict;
-}*/
+@synthesize manager;
 
-- (NSString*) dayOfTheWeek {
+- (NSString*) getDay{
     NSDate *date=[NSDate date];
     NSDateFormatter *dateFormatter=[[NSDateFormatter alloc]init];
     dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
@@ -87,8 +80,48 @@
     NSMutableArray* arr=[self setupArray];
     //NSMutableDictionary *dict=[self setupDictionary];
     //NSLog(@"%@", dict);
-    NSString *day=[self dayOfTheWeek];
+    NSMutableArray* days=@[@"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday"];
+    
+    NSString *day=[self getDay];
+    NSLog(@"%@", day);
+    NSMutableArray* times;
+    if([day isEqual:@"Saturday"]){
+        day=@"Monday";
+    }
+    else if([day isEqual:@"Sunday"]){
+        day=@"Monday";
+    }
+    int num=(int)[days indexOfObject:day];
+    times=arr[num];
+    [self homescreenView];
+    
 }
 
+- (void) homescreenView{
+    NSString* str=@"Find a parking lot!";
+    UILabel* label=[[UILabel alloc] init];
+    label.text=str;
+    label.textColor=UIColor.blackColor;
+    CGFloat width=200;
+    CGFloat height=100;
+    NSLog(@"%f", CGRectGetMidX(self.view.frame));
+    CGFloat x=CGRectGetMidX(self.view.frame)-(width/2);
+    //NSLog(@"%f", x);
+    CGFloat y=20;
+    label.frame=CGRectMake(x, y, width, height);
+    label.textAlignment=NSTextAlignmentCenter;
+    [self.view addSubview:label];
+    for(int i=0; i<self.view.subviews.count; i++){
+        NSLog(@"%@", self.view.subviews[i]);
+    }
+}
+
+- (void) startTrip {
+    manager=[[CLLocationManager alloc]init];
+    [manager startUpdatingLocation];
+    NSLog(manager);
+    CLLocation* location=manager.location;
+    //NSLog(location);
+}
 
 @end
